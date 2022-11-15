6cd708aca906c00408236949cbd6abcf6fc04376
coxde <63153334+coxde@users.noreply.github.com>
2022-11-15 14:29:06 +0800

    Add basic selectors and init conditions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const dice0El = document.querySelector('.dice--0');
+const dice1El = document.querySelector('.dice--1');
+const dice2El = document.querySelector('.dice--2');
+
+const btnNew = document.querySelector('.btn--new');
+const btnBig = document.querySelector('.btn--big');
+const btnSmall = document.querySelector('.btn--small');
+const btnRule = document.querySelector('.btn--rule');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+current0El.textContent = '...';
+current1El.textContent = '...';
